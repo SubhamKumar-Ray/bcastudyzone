c3ed870291980c4aeb61f0100fa7d8fade02d9e6
SUBHAM KUMAR RAY <sshubhamkumarray542@gmail.com>
2026-05-10 12:46:53 +0530

    Update dark.js

--- JS/dark.js
+++ JS/dark.js
@@ -3,3 +3,6 @@ function myFunction(){
     var element = document.body;
     element.classList.toggle("dark-mode");
 }
+function toggleDarkMode() {
+    document.body.classList.toggle("dark-mode");
+}
